d0b0c3d8b24d45a2251a788f092237f4eba9c84e
Hileene <92981899+Hileene@users.noreply.github.com>
2024-02-08 16:46:11 +0100

    Create the Colors Component file

---
+++ src/components/Colors/Colors.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './colors.css'
+
+//1. Display 3 colors: blue, green, pink
+//2. Hide colors
+//3. Display colors sequence
+//4. Colors sequence validation
+function Colors() {
+  return <div>Colors</div>
+}
+
+export default Colors
