572e9bf9fc2ff8add434fab4c69b9dcbe6b9c167
Hileene <92981899+Hileene@users.noreply.github.com>
2024-02-21 11:39:02 +0100

    Add container div to the button

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './start.css'
 
 function Start() {
-  return <button className="start-btn">Start</button>
+  return (
+    <div className="start-btn-container">
+      <button className="start-btn">Start</button>
+    </div>
+  )
 }
 
 export default Start
