92e48ac5ac475069bdbede5ef91ee378bc2ad307
Hileene <92981899+Hileene@users.noreply.github.com>
2024-02-21 11:02:30 +0100

    Add the Start component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Board from './components/Board/Board'
 import './App.css'
+import Start from './components/Start/Start'
 
 function App() {
   return (
     <div className="App">
       <Board />
+      <Start />
     </div>
   )
 }
