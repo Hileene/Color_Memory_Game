83ce7df49634d546842fda9f24d0f2e52b013a2f
Hileene <92981899+Hileene@users.noreply.github.com>
2024-03-07 13:53:48 +0100

    Create the Start button logic

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './start.css'
+import Colors from '../Colors/Colors'
 
 function Start() {
+  //Establish the state of the game if the game is the game has started or not
+  const [isGameStarted, setGameStarted] = useState(false)
+
+  // Create the click event handler function
+  const handleStartClick = () => {
+    // Upodate the state indicating that the game has started
+    setGameStarted(true)
+  }
   return (
     <div className="start-btn-container">
-      <button className="start-btn">Start</button>
+      {isGameStarted ? (
+        <Colors />
+      ) : (
+        <button onClick={handleStartClick} className="start-btn">
+          Start
+        </button>
+      )}
     </div>
   )
 }
