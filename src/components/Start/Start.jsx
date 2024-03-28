a989112b9addbfd35452caf266679212efe1179a
Hileene <92981899+Hileene@users.noreply.github.com>
2024-03-28 13:32:53 +0100

    Set up the start button

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -3,12 +3,12 @@ import './start.css'
 import Colors from '../Colors/Colors'
 
 function Start() {
-  //Establish the state of the game if the game is the game has started or not
+  //Establish the state of the game whether the game has started or not
   const [isGameStarted, setGameStarted] = useState(false)
 
   // Create the click event handler function
   const handleStartClick = () => {
-    // Upodate the state indicating that the game has started
+    // Update the state indicating that the game has started
     setGameStarted(true)
   }
   return (
