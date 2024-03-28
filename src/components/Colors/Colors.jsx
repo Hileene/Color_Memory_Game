d7ed07eea0a3bea7e4e3b4070eb9f8669c537eaa
Hileene <92981899+Hileene@users.noreply.github.com>
2024-03-28 13:44:57 +0100

    First draft of the Colors component

--- src/components/Colors/Colors.jsx
+++ src/components/Colors/Colors.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './colors.css'
 
-//1. Display 3 colors: blue, green, pink
-//2. Hide colors
-//3. Display colors sequence
-//4. Colors sequence validation
+//2. Generate Color Sequence
+//3. Display colors
+//4. Handle User Input
+//5. Check for Correct Sequence
+//6. Game Progression
+//7. User Feedback
 function Colors() {
+  //STATE MANAGEMENT:
+
+  //Establish the state of the current sequence of colors
+  const [currentSequence, setCurrentSequence] = useState()
+  //Establish the state of  the user input
+  const [userInput, setUserInput] = useState(false)
+
+  //GENERATE COLORS SEQUENCE:
+
+  // Define colors for the easy level
+  const easyColors = {
+    red: '#FF5733',
+    yellow: '#FFFF00',
+    green: '#00FF00',
+  }
+
+  // Define colors for the difficult level
+  const difficultColors = {
+    red: '#FF5733',
+    yellow: '#FFFF00',
+    green: '#00FF00',
+    blue: '#0000FF',
+    pink: '#FF69B4',
+  }
+
   return <div>Colors</div>
 }
 
